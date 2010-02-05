d0e226c29f05dda4d1b250a918fed04fb56c92b3
Jan Lehnardt <jan@apache.org>
2010-02-05 00:04:30 -0800

    poll rss feed to find the bugs that need updating instead of fetching all bugs

--- gigan.php
+++ gigan.php
@@ -12,71 +12,101 @@ $jira_attachment_url = "http://issues.apache.org/jira/secure/attachment/";
 
 // stop customising
 
-$xml = simplexml_load_file($xml_file_name);
+// ask gigan for the last updated bug's updated timestamp
+$couch = new CouchSimple(array("host" => $couchdb_host, "port" => $couchdb_port));
+
+echo "debug: getting latest update\n";
+$latest_update_result = $couch->send("GET", "/$db/_design/gigan/_view/latest-update?descending=true&limit=1");
+$latest_update_result = json_decode($latest_update_result);
+$latest_update = $latest_update_result->rows[0]->key;
+
+echo "debug: latest update: $latest_update\n";
+
+// read the JIRA RSS feed until it finds a date that is < that timestamp
+echo "getting the rss feed ...";
+$rss = file_get_contents("http://issues.apache.org/jira/sr/jira.issueviews:searchrequest-rss/temp/SearchRequest.xml?pid=12310780&sorter/field=issuekey&sorter/order=DESC&tempMax=10");
+echo "done\n";
+
+$xml = simplexml_load_string($rss, $class_name = "SimpleXMLElement", LIBXML_ERR_NONE);
 foreach($xml->channel->item AS $bug) {
-  $json = new stdClass();
-  $json->_id = (string)$bug->key;
-
- echo $json->_id . "...";
-
-  $json->title = (string)$bug->title;
-  $json->link = (string)$bug->link;
-  $json->description = (string)$bug->description;
-  $json->jira_key = (string)$bug->key["id"];
-  $json->summary = (string)$bug->summary;
-  $json->type = (string)$bug->type;
-  $json->priority = (string)$bug->priority;
-  $json->status = (string)$bug->status;
-  $json->resolution = (string)$bug->resolution;
-  $json->assignee->name = (string)$bug->assignee;
-  $json->assignee->id = (string)$bug->assignee["id"];
-  $json->reporter = (string)$bug->reporter;
-  $json->created = (string)$bug->created;
-  $json->updated = (string)$bug->updated;
-  $json->version = (string)$bug->version;
-  $json->comments = array();
-  if($bug->comments->comment) {
-    foreach($bug->comments->comment AS $comment) {
-      $json_comment = new stdClass();
-      $json_comment->id = (string)$comment["id"];
-      $json_comment->author = (string)$comment["author"];
-      $json_comment->created = (string)$comment["created"];
-      $json_comment->comment = (string)$comment;
-      $json->comments[] = $json_comment;
-    }
+  $date = strtotime((string)$bug->pubDate) . "000";
+  echo "comparing $date with $latest_update\n";
+  if($date <= $latest_update) {
+    break;
   }
+  $link = (string)$bug->link;
+  $_split = explode("/", $link);
+  $id = $_split[count($_split)-1];
 
-  if($bug->attachments->attachment) {
-    foreach($bug->attachments->attachment AS $attachment) {
-      $json_attachment = new stdClass();
-      $json_attachment->id = (string)$attachment["id"];
-      $json_attachment->name = (string)$attachment["name"];
-      $json_attachment->size = (string)$attachment["size"];
-      $json_attachment->created = (string)$attachment["created"];
-      $json_attachment->content_type = "text/plain";
-      $json_attachment->data = base64_encode(file_get_contents("{$jira_attachment_url}/{$json_attachment->id}/{$json_attachment->name}"));
-      $json->_attachments->{$json_attachment->name} = $json_attachment;
-    }
-  }
+  echo "fetching $id...";
 
-  // echo json_encode($json);
-  $couch = new CouchSimple(array("host" => $couchdb_host, "port" => $couchdb_port));
-
-  // get rev
-  $res = $couch->send("GET", "/$db/$json->_id");
-  if($res) {
-    $doc = json_decode($res);
-    if($doc->_rev) {
-      $json->_rev = $doc->_rev;
-    }
-    if($doc->couchdb_fields) {
-      $json->couchdb_fields = $doc->couchdb_fields;
-    }
-  }
-
-  $couch->send("PUT", "/$db/$json->_id", json_encode($json));
+  $xml = simplexml_load_file("http://issues.apache.org/jira/si/jira.issueviews:issue-xml/$id/$id.xml", $class_name = "SimpleXMLElement", LIBXML_ERR_NONE);
   echo "done\n";
-  // exit();
+  echo "parsing...";
+  foreach($xml->channel->item AS $bug) {
+    $json = new stdClass();
+    $json->_id = (string)$bug->key;
+
+    echo $json->_id . "...";
+
+    $json->title = (string)$bug->title;
+    $json->link = (string)$bug->link;
+    $json->description = (string)$bug->description;
+    $json->jira_key = (string)$bug->key["id"];
+    $json->summary = (string)$bug->summary;
+    $json->type = (string)$bug->type;
+    $json->priority = (string)$bug->priority;
+    $json->status = (string)$bug->status;
+    $json->resolution = (string)$bug->resolution;
+    $json->assignee->name = (string)$bug->assignee;
+    $json->assignee->id = (string)$bug->assignee["id"];
+    $json->reporter = (string)$bug->reporter;
+    $json->created = (string)$bug->created;
+    $json->updated = (string)$bug->updated;
+    $json->version = (string)$bug->version;
+    $json->comments = array();
+    if($bug->comments->comment) {
+      foreach($bug->comments->comment AS $comment) {
+        $json_comment = new stdClass();
+        $json_comment->id = (string)$comment["id"];
+        $json_comment->author = (string)$comment["author"];
+        $json_comment->created = (string)$comment["created"];
+        $json_comment->comment = (string)$comment;
+        $json->comments[] = $json_comment;
+      }
+    }
+
+    if($bug->attachments->attachment) {
+      foreach($bug->attachments->attachment AS $attachment) {
+        $json_attachment = new stdClass();
+        $json_attachment->id = (string)$attachment["id"];
+        $json_attachment->name = (string)$attachment["name"];
+        $json_attachment->size = (string)$attachment["size"];
+        $json_attachment->created = (string)$attachment["created"];
+        $json_attachment->content_type = "text/plain";
+        $json_attachment->data = base64_encode(file_get_contents("{$jira_attachment_url}/{$json_attachment->id}/{$json_attachment->name}"));
+        $json->_attachments->{$json_attachment->name} = $json_attachment;
+      }
+    }
+
+    // echo json_encode($json);
+    echo "done\n";
+    // get rev
+    $res = $couch->send("GET", "/$db/$json->_id");
+    if($res) {
+      $doc = json_decode($res);
+      if($doc->_rev) {
+        $json->_rev = $doc->_rev;
+      }
+      if($doc->couchdb_fields) {
+        $json->couchdb_fields = $doc->couchdb_fields;
+      }
+    }
+
+    $couch->send("PUT", "/$db/$json->_id", json_encode($json));
+    echo "done\n";
+    // exit();
+  }
 }
 
 // classes
